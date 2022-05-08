3687a07f563b97e334a12b61b83d3dca59226836
Anh Pham <anhpt379@gmail.com>
2022-05-08 07:15:39 +0200

    style: fix linter warnings

--- lib/puppet-lint/plugins/check_class_params_alignment.rb
+++ lib/puppet-lint/plugins/check_class_params_alignment.rb
@@ -3,55 +3,49 @@
 #
 # https://puppet.com/docs/puppet/7/style_guide.html#style_guide_classes-param-indentation-alignment
 
-require 'debug'
-
 def a_param?(token)
   if token.prev_code_token.type == :EQUALS
     false
   elsif token.prev_code_token.type == :FARROW
     false
-  elsif token.type == :VARIABLE and not [:DQPRE, :DQMID].include?(token.prev_code_token.type)
+  elsif token.type == :VARIABLE && !%i[DQPRE DQMID].include?(token.prev_code_token.type)
     true
   end
 end
 
 def first_param_on_the_line?(token)
   origin = token
-  while token and token.prev_token
+  while token&.prev_token
     token = token.prev_token
 
     break if token.type == :NEWLINE
   end
 
-  while token and token.next_token
+  while token&.next_token
     token = token.next_token
 
-    if token.type == :VARIABLE
-      break
-    end
+    break if token.type == :VARIABLE
   end
 
-  if origin == token
-    true
-  else
-    false
-  end
+  origin == token
 end
 
 def the_one?(token, character)
-  if character == '='
+  case character
+  when '='
     true if token.type == :EQUALS
-  elsif character == '$'
-    true if token.type == :VARIABLE and a_param?(token) and first_param_on_the_line?(token)
+  when '$'
+    true if token.type == :VARIABLE && a_param?(token) && first_param_on_the_line?(token)
   end
 end
 
 def get_prev_code_token(token, character)
-  if character == '='
+  case character
+  when '='
     token.prev_code_token
-  elsif character == '$'
+  when '$'
     if token.prev_code_token
-      if [:CLASSREF, :RBRACK].include?(token.prev_code_token.type)
+      if %i[CLASSREF RBRACK].include?(token.prev_code_token.type)
         token.prev_code_token
       elsif token.prev_code_token.type == :LPAREN
         token
@@ -69,7 +63,7 @@ end
 
 # This function is copied & modified from puppet-lint arrow_alignment check
 # https://github.com/puppetlabs/puppet-lint/blob/020143b705b023946739eb44e7c7d99fcd087527/lib/puppet-lint/plugins/check_whitespace/arrow_alignment.rb#L8
-def _check(character)
+def check_for(character)
   # I intentionally didn't rename `arrow` to another name, to keep the code as
   # similar as the original one, to easier to update in the future.
   (class_indexes + defined_type_indexes).each do |res_idx|
@@ -97,26 +91,18 @@ def _check(character)
 
         param_length = param_token.to_manifest.length
 
-        if param_column[level_idx].nil?
-          param_column[level_idx] = param_token.column
-        end
+        param_column[level_idx] = param_token.column if param_column[level_idx].nil?
 
         if (level_tokens[level_idx] ||= []).any? { |t| t.line == token.line }
           this_arrow_column = param_column[level_idx] + param_length + 1
+        elsif character == '$' && param_token.type == :VARIABLE
+          this_arrow_column = param_token.column
         else
-          if character == '$' and param_token.type == :VARIABLE
-            this_arrow_column = param_token.column
-          else
-            this_arrow_column = param_token.column + param_token.to_manifest.length
-            if param_token.type != :INDENT
-              this_arrow_column += 1
-            end
-          end
+          this_arrow_column = param_token.column + param_token.to_manifest.length
+          this_arrow_column += 1 if param_token.type != :INDENT
         end
 
-        if arrow_column[level_idx] < this_arrow_column
-          arrow_column[level_idx] = this_arrow_column
-        end
+        arrow_column[level_idx] = this_arrow_column if arrow_column[level_idx] < this_arrow_column
 
         (level_tokens[level_idx] ||= []) << token
       elsif token.prev_token.type == :LPAREN
@@ -132,13 +118,13 @@ def _check(character)
             arrows_on_line = level_tokens[level_idx].select { |t| t.line == arrow_tok.line }
             notify(
               :warning,
-              :message        => "indentation of #{character} is not properly aligned (expected in column #{arrow_column[level_idx]}, but found it in column #{arrow_tok.column})",
-              :line           => arrow_tok.line,
-              :column         => arrow_tok.column,
-              :token          => arrow_tok,
-              :arrow_column   => arrow_column[level_idx],
-              :newline        => arrows_on_line.index(arrow_tok) != 0,
-              :newline_indent => param_column[level_idx] - 1
+              message: "indentation of #{character} is not properly aligned (expected in column #{arrow_column[level_idx]}, but found it in column #{arrow_tok.column})",
+              line: arrow_tok.line,
+              column: arrow_tok.column,
+              token: arrow_tok,
+              arrow_column: arrow_column[level_idx],
+              newline: arrows_on_line.index(arrow_tok) != 0,
+              newline_indent: param_column[level_idx] - 1
             )
           end
         end
@@ -153,7 +139,7 @@ end
 
 # This function is copied & modified from puppet-lint arrow_alignment fix
 # https://github.com/puppetlabs/puppet-lint/blob/020143b705b023946739eb44e7c7d99fcd087527/lib/puppet-lint/plugins/check_whitespace/arrow_alignment.rb#L94
-def _fix(problem)
+def fix_for(problem)
   if problem[:newline]
     index = tokens.index(problem[:token].prev_code_token.prev_token)
 
@@ -165,25 +151,25 @@ def _fix(problem)
     problem[:token].prev_code_token.prev_token.value = ' ' * problem[:newline_indent]
 
     end_param_idx = tokens.index(problem[:token].prev_code_token)
-    start_param_idx = tokens.index(problem[:token].prev_token_of([:INDENT, :NEWLINE]))
-    param_length = tokens[start_param_idx..end_param_idx].map { |r| r.to_manifest.length }.reduce(0) { |sum, x| sum + x } + 1
+    start_param_idx = tokens.index(problem[:token].prev_token_of(%i[INDENT NEWLINE]))
+    param_length = tokens[start_param_idx..end_param_idx].map do |r|
+                     r.to_manifest.length
+                   end.reduce(0) { |sum, x| sum + x } + 1
     new_ws_len = problem[:arrow_column] - param_length
   else
     new_ws_len = if problem[:token].prev_token.type == :WHITESPACE
-                    problem[:token].prev_token.to_manifest.length
-                  else
-                    0
-                  end
+                   problem[:token].prev_token.to_manifest.length
+                 else
+                   0
+                 end
     new_ws_len += (problem[:arrow_column] - problem[:token].column)
   end
 
-  if new_ws_len < 0
-    if problem[:token].prev_token.type == :INDENT
-      new_ws = problem[:token].prev_token.to_manifest[0...new_ws_len]
-      problem[:token].prev_token.value = new_ws
-    else
-      raise PuppetLint::NoFix
-    end
+  if new_ws_len.negative?
+    raise PuppetLint::NoFix if problem[:token].prev_token.type != :INDENT
+
+    new_ws = problem[:token].prev_token.to_manifest[0...new_ws_len]
+    problem[:token].prev_token.value = new_ws
   else
     new_ws = ' ' * new_ws_len
 
@@ -198,20 +184,20 @@ end
 
 PuppetLint.new_check(:class_params_alignment) do
   def check
-    _check('$')
+    check_for('$')
   end
 
   def fix(problem)
-    _fix(problem)
+    fix_for(problem)
   end
 end
 
 PuppetLint.new_check(:class_equals_alignment) do
   def check
-    _check('=')
+    check_for('=')
   end
 
   def fix(problem)
-    _fix(problem)
+    fix_for(problem)
   end
 end
